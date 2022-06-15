f9bcafe37d7b7a7432c56a974f57ceada111a0d0
Spud97 <luca.dercole97@gmail.com>
2022-06-15 18:30:43 +0200

    Milestone 3 svolta

--- Milestone 3/js/db.js
+++ Milestone 3/js/db.js
@@ -180,27 +180,34 @@ const appVue = new Vue({
       console.log(activeUser);
     },
 
-    autoAnswer(messages, message) {
-      this.messages.push({
-        message: "Ok",
-      });
-    },
-
-    addMessage(autoAnswer, messages, message, newMessageText, status, activeUser) {
+    addMessage(
+      autoAnswer,
+      messages,
+      message,
+      newMessageText,
+      status,
+      activeUser
+    ) {
       const newMessage = this.newMessageText.trim();
 
       if (newMessage === "") {
         return;
       }
 
-      this.messages.push({
+      this.activeUser.messages.push({
         message: newMessage,
         status: "sent",
       });
 
       this.newMessageText = "";
 
-      const myTimeout = setTimeout(autoAnswer(), 1000);
+      setTimeout(() => this.autoAnswer(), 1000);
+    },
+
+    autoAnswer(messages, message, activeUser) {
+      this.activeUser.messages.push({
+        message: "Ok",
+      });
     },
   },
 });
